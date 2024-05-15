3169f7798b1ec39a779e746b1a92ee0a7477d233
Zakariae ABDOUNI <abdouni.zakariae@gmail.com>
2024-05-15 11:43:38 +0100

    Initial commit

---
+++ src/test/java/ma/zakariae/abdouni/GrpcEtMicroServiceMultiConnecteursGrpcSoapGraphQlRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.zakariae.abdouni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcEtMicroServiceMultiConnecteursGrpcSoapGraphQlRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
